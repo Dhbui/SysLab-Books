8a18aa38f2f397bf96da8c9b15d08f3edcc11ce4
Dylan <dhbui16@gmail.com>
2019-05-29 09:44:30 -0400

    Indexing for Neural Network completed

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -8,19 +8,72 @@ titles = [("MiddleEnglishBookTitles.txt", "Middle English"), ("RenaissanceBookTi
 X = []
 Y = []
 
+def runNeuralNet():
+    for titleTuple in titles:
+        f = open(titleTuple[0], 'r')
+        lines = f.readlines()
+        f.close()
+        for line in lines:
+            temp = line
+            if temp[-1] == "\n":
+                temp = line[:-1]
+            f = open("C:\\Users\\Dylan\\Documents\\SysLab Books\\" + titleTuple[1] + "\\" + temp + ".txt", 'r')
+            X.append(" ".join(f.readlines()))
+            Y.append(titleTuple[1])
 
-for titleTuple in titles:
-    f = open(titleTuple[0], 'r')
-    lines = f.readlines()
+
+def createIndexing():
+    toKeep = "abcdefghijklmnopqrstuvwxyz1234567890"
+    indexMap = dict()
+    index = 0
+    for title in titles:
+        f = open("C:\\Users\\Dylan\\Documents\\SysLab Books\\" + title[0], 'r')
+        books = f.readlines()
+        f.close()
+        for book in books:
+            book1 = book
+            if book[-1] == "\n":
+                book1 = book[:-1]
+            f = open("C:\\Users\\Dylan\\Documents\\SysLab Books\\" + title[1] + "\\" + book1 + ".txt", 'r')
+            text = f.readlines()
+            toRemove = ""
+            for line in text:
+                for char in line:
+                    if char not in toKeep:
+                        toRemove = toRemove + char
+                temp = line
+                for char in toRemove:
+                    temp.replace(char, " ")
+                temp = temp.split(" ")
+                if "" in temp:
+                    temp.remove("")
+                for word in temp:
+                    temp2 = word
+                    print(temp2)
+                    if temp2[-1] == '\n':
+                        temp2 = word[:-1]
+                    if temp2 not in indexMap:
+                        indexMap[word] = index
+                        index += 1
+    f = open("C:\\Users\\Dylan\\Documents\\SysLab Books\\Data\\indexMapping.txt", 'w+')
+    for key in indexMap:
+        f.write(str(indexMap[key]) + " " + key + "\n")
     f.close()
-    for line in lines:
-        temp = line
-        if temp[-1] == "\n":
-            temp = line[:-1]
-        f = open("C:\\Users\\Dylan\\Documents\\SysLab Books\\" + titleTuple[1] + "\\" + temp + ".txt", 'r')
-        X.append(" ".join(f.readlines()))
-        Y.append(titleTuple[1])
 
+def getIndexing():
+    indexMap = dict()
+    f = open("indexMapping.txt", 'r')
+    mapping = f.readlines()
+    f.close()
+    for pair in mapping:
+        line = pair
+        if pair == "":
+            break
+        if pair[-1] == "\n":
+            line = pair[:-1]
+        item = line.split(" ")
+        indexMap[line[1]] = int(line[0])
+    return indexMap
 
-
+createIndexing()
 
